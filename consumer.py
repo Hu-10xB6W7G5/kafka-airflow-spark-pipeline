969544bbf0a91810fc3fef66400df60d8350fd87
Yohans Samuel <yohanssamuel2014@gmail.com>
2022-10-05 17:42:34 +0300

    add consumer

---
+++ consumer.py
@@ -0,0 +1,15 @@
+from kafka import KafkaConsumer
+
+# To consume latest messages and auto-commit offsets
+consumer = KafkaConsumer('g5-topic-1',
+                         group_id='my-group',
+                        # b-1.batch6w7.6qsgnf.c19.kafka.us-east-1.amazonaws.com:9092,
+                        # b-2.batch6w7.6qsgnf.c19.kafka.us-east-1.amazonaws.com:9092
+                         bootstrap_servers=['localhost:9092'])
+
+for message in consumer:
+    # message value and key are raw bytes -- decode if necessary!
+    # e.g., for unicode: `message.value.decode('utf-8')`
+    print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
+                                          message.offset, message.key,
+                                          message.value))
